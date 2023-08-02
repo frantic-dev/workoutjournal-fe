28d246d9a4f079920ea098083137019d9b67221e
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-08-02 22:31:03 +0100

    commiting complete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react'
+import Form from './components/Form'
+
 function App() {
-return(
-  <div>hello world</div>
-)
+	const [workouts, setWorkouts] = useState([])
+	const [workout, setWorkout] = useState({
+		url: '',
+	})
+
+  function changeWorkout(e) {
+    setWorkout(workout => ({...workout, [e.target.name]: e.target.value }))
+  }
+	return (
+		<div>
+			<Form workout={workout} changeWorkout={changeWorkout} />
+		</div>
+	)
 }
 
 export default App
+
